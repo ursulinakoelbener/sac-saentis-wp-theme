d815a840b552f9586f1d7db5d479a41addd194bf
Basil Brunner <bbrunner@extron.com>
2016-09-09 20:18:07 +0200

    Add footer image

--- footer.php
+++ footer.php
@@ -1,5 +1,6 @@
             </div> <!-- end:content_wrapper -->
         </div> <!-- end:content -->
+        <img src="<?php bloginfo('template_directory'); ?>/images/footer.png" id="footer">
         <?php //get_sidebar( 'footer-top' );
         //get_sidebar( 'footer-bottom' );
         wp_footer(); ?>
