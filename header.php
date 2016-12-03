79b96f8ff32792eeefc9d28f3f8e801ae102c37a
Basil Brunner <bbrunner@extron.com>
2016-12-03 12:20:46 +0100

    Update styles latest version

--- header.php
+++ header.php
@@ -14,13 +14,13 @@
     <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
     <![endif]-->
 
-    <button class="c-hamburger c-hamburger--htx" id="btn_nav"><span>toggle menu</span></button>
-    <div id="nav">
+    <button class="sacsaentis-nav-toggle c-hamburger c-hamburger--htx"><span>toggle menu</span></button>
+    <div class="sacsaentis-nav-pane">
         <?php wp_nav_menu( array(
             'theme_location' => 'main-menu',
             'container' => '',
             'fallback_cb' => '__return_false'
         ) ); ?>
-        <div class="line"><hr></div>
+        <hr class="sacsaentis-nav-pane__separator">
         <a class="btn_fb" target="_blank" href="https://www.facebook.com/sacsaentis/"><img src="<?php bloginfo('template_directory'); ?>/images/icon_fb.png"></a>
     </div>
